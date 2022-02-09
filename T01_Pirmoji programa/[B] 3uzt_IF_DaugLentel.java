bfbe01adbe126f7fb011be608825c557fc15f889
Taironas <94172076+taironasdirse@users.noreply.github.com>
2022-02-09 13:20:26 +0200

    Rename [B] 3uzt_IF_DaugLentel.txt to [B] 3uzt_IF_DaugLentel.java

--- T01_Pirmoji programa/[B] 3uzt_IF_DaugLentel.java
+++ T01_Pirmoji programa/[B] 3uzt_IF_DaugLentel.java
@@ -5,12 +5,12 @@ import java.util.Scanner; //Biblioteka scanner
 public class Main {
     public static void main(String[] args) {
 	    int sk1, ats;                               //Kintamieji
-        System.out.println("Daugybos Lentelë");
-        System.out.println("Áveskite Skaièiø: ");
+        System.out.println("Daugybos LentelÃ«");
+        System.out.println("Ãveskite SkaiÃ¨iÃ¸: ");
         Scanner S = new Scanner(System.in);
         sk1 = S.nextInt();                          //Ivedimui
         for(int h = 1; h<11; h++){
-            ats = sk1 * h;                          //Daugybos lentelë
+            ats = sk1 * h;                          //Daugybos lentelÃ«
             // System.out.println(h + " * " + sk1 + " = " + ats);
             if(ats < 50){
                 System.out.println(sk1+" * "+h+" = "+ats+" < 50");
